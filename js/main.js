787dd969a5fdc1d62bd2cb4435369743ed9913e9
Liza George <liza@chiramattel.com>
2021-06-17 01:15:10 -0700

    Got the hours calculation!
    Consulted this article for help in converting the millisecond difference to a readable format: https://www.tutorialspoint.com/How-to-get-time-difference-between-two-timestamps-in-seconds

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function logFunction(){
-    var sleptTime = document.getElementById("sleptTime").value;
-    var wokeTime = document.getElementById("wokeTime").value;
+    var sleptTime = new Date(document.getElementById("sleptTime").value+":00");
+    var wokeTime = new Date(document.getElementById("wokeTime").value+":00");
 
 
     if(sleptTime == '' && wokeTime == ''){
@@ -11,11 +11,24 @@ function logFunction(){
     }
     else if(wokeTime == ''){
         alert('Please select the time you woke up!');
-    }
+        //TODO: handle invalid inputs of times, like is wake up is before sleep
+    } 
     else{
         console.log('slept ' + sleptTime + ' to ' + wokeTime);
-        var timeSlept = "";
+        
+        var timeSleptRough = wokeTime.getTime()-sleptTime.getTime();
+
+        //**CREDIT: I looked at this article for help at this stage; https://www.tutorialspoint.com/How-to-get-time-difference-between-two-timestamps-in-seconds
+
+        var sec = timeSleptRough/1000;
+        var days = Math.floor(sec/86400);
+        var hours = Math.floor(sec/3600) %24 + days*24;
+        var mins = Math.floor(sec/60) % 60;
+
+        var timeSlept = hours + " hours, " + mins + " minutes."
         document.getElementById("time-slept").innerHTML = timeSlept;
+
+        //TODO: check error "Unchecked runtime.lastError: The message port closed before a response was received."
     }
 
 }
